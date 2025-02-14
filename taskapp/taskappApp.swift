3f9f57b889b8e44ac22eb304ffca9fbf604affc7
Fidel Villanueva <fvillanueva.pe@gmail.com>
2025-02-14 09:09:25 -0500

    Initial Commit

---
+++ taskapp/taskappApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskappApp.swift
+//  taskapp
+//
+//  Created by Fidel Villanueva Delgado on 14/02/25.
+//
+
+import SwiftUI
+
+@main
+struct taskappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ taskappTests/taskappTests.swift
@@ -0,0 +1,17 @@
+//
+//  taskappTests.swift
+//  taskappTests
+//
+//  Created by Fidel Villanueva Delgado on 14/02/25.
+//
+
+import Testing
+@testable import taskapp
+
+struct taskappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
