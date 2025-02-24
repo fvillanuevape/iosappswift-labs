bed1fcf17776378fe59d44d2f4fd2ba5b2721610
Fidel Villanueva <fvillanueva.pe@gmail.com>
2025-02-23 21:59:21 -0500

    Add button component.

--- taskapp/IMC/IMCView.swift
+++ taskapp/IMC/IMCView.swift
@@ -16,6 +16,9 @@ struct IMCView: View {
     }
      */
     @State var gender: Int = 0
+    @State var height: Double = 150
+    @State var age: Int = 18
+    @State var weight: Int = 80
     
     var body: some View {
         VStack{
@@ -23,6 +26,12 @@ struct IMCView: View {
                 ToggleButtonComponent(text: "Male", imageName: "heart.fill", gender: 0, genderSelected: $gender)
                 ToggleButtonComponent(text: "Female", imageName: "star.fill", gender: 1, genderSelected: $gender)
             }
+            HeightCalculatorComponent(selectedHeight: $height)
+            HStack{
+                CounterComponent(titleCounter: "Age", numberCounter: $age)
+                CounterComponent(titleCounter: "Weight", numberCounter: $weight)
+            }
+            
             
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -91,6 +100,93 @@ struct InformationComponent: View {
             .font(.largeTitle)
             .bold()
             .foregroundColor(.white)
+    }
+}
+
+struct TitleComponent: View {
+    
+    let textTitle: String
+    
+    var body: some View {
+        Text(textTitle)
+            .font(.title2)
+            .foregroundColor(.gray)
+    }
+}
+
+struct HeightCalculatorComponent: View {
+    
+    @Binding var selectedHeight: Double
+    var body: some View {
+        VStack{
+            TitleComponent(textTitle: "Altura")
+            InformationComponent(textInformation: "\(Int(selectedHeight)) cm")
+            Slider(
+                value: $selectedHeight,
+                in: 100...200,
+                step: 1
+            )
+            .accentColor(.purple)
+            .padding(.horizontal, 16)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.backgroundComponent)
+    }
+}
+
+struct CounterComponent: View {
+    let titleCounter: String
+    @Binding var numberCounter: Int
+    
+    var body: some View {
+        
+        VStack{
+            TitleComponent(textTitle: titleCounter)
+            InformationComponent(textInformation: String(numberCounter))
+            HStack{
+                Button(action: {
+                    
+                    if (numberCounter > 0){
+                        numberCounter -= 1
+                    }
+                    
+                })
+                {
+                    ZStack
+                    {
+                        Circle()
+                            .frame(width: 70, height: 70)
+                            .foregroundColor(.purple)
+                        Image(systemName: "minus")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(.white)
+                            .frame(width: 25, height: 25)
+                    }
+                }
+                Button(action: {
+                    
+                    if (numberCounter < 100){
+                        numberCounter += 1
+                    }
+                    
+                }){
+                    ZStack
+                    {
+                        Circle()
+                            .frame(width: 70, height: 70)
+                            .foregroundColor(.purple)
+                        Image(systemName: "plus")
+                            .resizable()
+                            .foregroundColor(.white)
+                            .frame(width: 25, height: 25)
+                    }
+                }
+            }
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.backgroundComponent)
     }
 }
 
